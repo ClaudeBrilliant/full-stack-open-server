f12c75b277bb7c469408d609e8763c7684c2219c
Claude Brilliant <claudennyongesa7@gmail.com>
2026-03-11 20:20:59 +0300

    fix: rewrite taskMiddleware to use Mongoose

--- server/src/middlewares/taskMiddleware.js
+++ server/src/middlewares/taskMiddleware.js
@@ -1,19 +1,17 @@
-const db = require('../db.json');
-
-let tasks = db.tasks;
+const { Task } = require('../../mongo.js');
 
 // Middleware to find a task by ID
-const findTaskById = (req, res, next) => {
-  const id = parseInt(req.params.id);
-  const task = tasks.find(t => t.id === id);
-  if (!task) {
-    return res.status(404).json({ error: 'Task not found' });
+const findTaskById = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+    req.task = task;
+    next();
+  } catch (err) {
+    return res.status(400).json({ error: 'Invalid ID format' });
   }
-  req.task = task;
-  req.taskIndex = tasks.findIndex(t => t.id === id);
-  next();
 };
 
-module.exports = {
-  findTaskById
-};
+module.exports = { findTaskById };
